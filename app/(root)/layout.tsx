39eeb7cf9c1b80af6581a30d3ef99933a80f042d
Ishika-Raghuvanshi <ishika21303069@gmail.com>
2024-08-03 20:43:29 +0530

    hello

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,25 @@
+import StreamVideoProvider  from '@/providers/StreamClientProvider'
+import React, { ReactNode } from 'react'
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "YOOM",
+  description: "Video calling App",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
+
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+  return (
+   <main>
+    <StreamVideoProvider>
+    {children}
+    </StreamVideoProvider>
+    
+   </main>
+  )
+}
+
+export default RootLayout
